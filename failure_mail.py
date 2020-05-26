16d89258e9d69ddc80caeab8bd06002879fed546
yobahBertrandYonkou <bmbvfx@gmail.com>
2020-05-26 18:35:09 +0530

    Files

---
+++ failure_mail.py
@@ -0,0 +1,9 @@
+li = [recipients_mail_ids]
+
+for dest in li:
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(sender's_mail_id, sender's_password)
+    message = "Model accuracy failed target"
+    s.sendmail(sender's_mail_id, dest, message)
+    s.quit()
